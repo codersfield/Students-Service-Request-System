d7f05166b4b801bf87e6889edce66d9d586404ea
adhavalboy <dhaval.daiict@gmail.com>
2018-09-02 13:59:46 +0530

    front-end

--- js/style.js
+++ js/style.js
@@ -1,1 +1,22 @@
  
+$(document).ready(function() {
+
+    // import view port library function
+    jQuery.extend(verge);
+  
+    // smooth scrolling of viewport to target
+    $('a[href^="#"]').click(function(event) {
+      event.preventDefault();
+      $('html,body').animate({
+        scrollTop: $(this.hash).offset().top - 50
+      }, 900);
+    });
+  
+    $('#down').click(function() {
+      $('html,body').animate({
+        scrollTop: $('#about').offset().top - 50
+      }, 900);
+    });
+  
+    
+  });
