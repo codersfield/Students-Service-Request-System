8597023615dc55b272641791e4ed6015c140b1a4
ST2-EV <40699517+ST2-EV@users.noreply.github.com>
2018-10-18 11:51:19 +0530

    added a basic signup page

---
+++ page/signup.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Sign UP!</title>
+  </head>
+  <body>
+		<nav class="navbar navbar-dark bg-dark">
+				<a class="navbar-brand" href="#">Sign Up Form</a>
+		</nav>
+			  
+		<form class="container">
+				<div class="form-group">
+				  <label for="firstname">First Name</label>
+				  <input type="text" class="form-control" id="firstname" aria-describedby="emailHelp" placeholder="first name">
+				</div>
+				<div class="form-group">
+				  <label for="lastname">Last name</label>
+				  <input type="text" class="form-control" id="lastname" placeholder="last name">
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" class="form-control" id="email" placeholder="Email">
+				</div>
+				<div class="form-group">
+					<label for="uniname">University name</label>
+					<input type="text" class="form-control" id="uniname" placeholder="University name">
+				</div>
+				<div class="form-group">
+					<label for="padd">Permanent address</label>
+					<input type="text" class="form-control" id="padd" placeholder="Permanent address">
+				</div>
+				<div class="form-group">
+						<label for="num">Telephone</label>
+						<input type="number" class="form-control" id="num" placeholder="number">
+				</div>
+				
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
